e49dc9822cce9ea491d82ead4a02f0c4728a10b5
myron0220 <WXYZ0daniu@gmail.com>
2022-05-15 12:41:51 -0400

    implement CRUD in backend (Springboot)

--- backend/src/main/java/com/mingzhe/backend/controller/SysUserController.java
+++ backend/src/main/java/com/mingzhe/backend/controller/SysUserController.java
@@ -1,8 +1,12 @@
 package com.mingzhe.backend.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mingzhe.backend.entity.SysUser;
 import com.mingzhe.backend.mapper.SysUserMapper;
 import com.mingzhe.backend.service.SysUserService;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,43 +18,55 @@ import java.util.Map;
 @RequestMapping("/sys-user")
 public class SysUserController {
     @Autowired
-    private SysUserMapper userMapper;
-
-    @Autowired
     private SysUserService userService;
 
-    // add and update
+    // C U (create or update)
     @PostMapping
     public boolean save(@RequestBody SysUser sysUser) {
         // add new or update old
         return userService.saveUser(sysUser);
     }
 
-    // query
+    // R (read all)
     @GetMapping
-    public List<SysUser> index() {
-/* debug test */
-//        List<SysUser> all = userMapper.findAll();
-//        return userMapper.findAll();
-        return userMapper.findAll();
+    public List<SysUser> findAll() {
+        return userService.list();
     }
 
+    // D (delete)
     @DeleteMapping({"/{id}"})
-    public Integer delete(@PathVariable Integer id) {
-        return userMapper.deleteById(id);
+    public boolean delete(@PathVariable Integer id) {
+        return userService.removeById(id);
     }
 
+    // R (read by condition)
+    // pagination
     @GetMapping("/page")
-    public Map<String, Object> findPage(@RequestParam Integer pageNum,
-                                        @RequestParam Integer pageSize,
-                                        @RequestParam String username) {
-        pageNum = (pageNum - 1) * pageSize;
-        username = "%" + username + "%";
-        Map<String, Object> res = new HashMap<>();
-        Integer total = userMapper.selectTotal(username);
-        List<SysUser> data = userMapper.selectPage(pageNum, pageSize, username);
-        res.put("total", total);
-        res.put("data", data);
-        return res;
+    public IPage<SysUser> findPage(@RequestParam Integer pageNum,
+                                   @RequestParam Integer pageSize,
+                                   @RequestParam(defaultValue =  "") String username,
+                                   @RequestParam(defaultValue =  "") String nickname,
+                                   @RequestParam(defaultValue =  "") String address) {
+        IPage<SysUser> page = new Page<>(pageNum, pageSize);
+        QueryWrapper<SysUser> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like(Strings.isNotEmpty(username),"username", username);
+        // default add AND by framework
+        queryWrapper.like(Strings.isNotEmpty(nickname),"nickname", nickname);
+        // or test
+        queryWrapper.or().like(Strings.isNotEmpty(address),"address", address);
+        return userService.page(page, queryWrapper);
     }
+//    @GetMapping("/page")
+//    public Map<String, Object> findPage(@RequestParam Integer pageNum,
+//                                        @RequestParam Integer pageSize,
+//                                        @RequestParam String username) {
+//        pageNum = (pageNum - 1) * pageSize;
+//        username = "%" + username + "%";
+//        Map<String, Object> res = new HashMap<>();
+//        Integer total = userMapper.selectTotal(username);
+//        List<SysUser> data = userMapper.selectPage(pageNum, pageSize, username);
+//        res.put("total", total);
+//        res.put("data", data);
+//        return res;
+//    }
 }
--- backend/src/main/java/com/mingzhe/backend/mapper/SysUserMapper.java
+++ backend/src/main/java/com/mingzhe/backend/mapper/SysUserMapper.java
@@ -3,28 +3,30 @@ package com.mingzhe.backend.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.mingzhe.backend.entity.SysUser;
 import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
+@Mapper
 public interface SysUserMapper extends BaseMapper<SysUser> {
-    @Select("SELECT * FROM sys_user")
-    List<SysUser> findAll();
-
-//    @Insert("INSERT INTO SpringVueMSDB.sys_user " +
-//            "(username, password, nickname, email, phone, address) " +
-//            "VALUES (#{username}, #{password}, #{nickname}, #{email}, #{phone}, #{address})")
-//    Integer insert(SysUser user);
-
-    // mapping is defined in src/mapper/SysUser.xml to achieve dynamic update
-    Integer update(SysUser sysUser);
-
-    @Delete("DELETE FROM sys_user WHERE id = #{id}")
-    Integer deleteById(Integer id);
-
-    @Select("SELECT * FROM sys_user WHERE username LIKE #{username} LIMIT #{pageNum}, #{pageSize}")
-    List<SysUser> selectPage(Integer pageNum, Integer pageSize, String username);
-
-    @Select("SELECT COUNT(*) FROM sys_user WHERE username LIKE #{username}")
-    Integer selectTotal(String username);
+//    @Select("SELECT * FROM sys_user")
+//    List<SysUser> findAll();
+//
+////    @Insert("INSERT INTO SpringVueMSDB.sys_user " +
+////            "(username, password, nickname, email, phone, address) " +
+////            "VALUES (#{username}, #{password}, #{nickname}, #{email}, #{phone}, #{address})")
+////    Integer insert(SysUser user);
+//
+//    // mapping is defined in src/mapper/SysUser.xml to achieve dynamic update
+//    Integer update(SysUser sysUser);
+//
+//    @Delete("DELETE FROM sys_user WHERE id = #{id}")
+//    Integer deleteById(Integer id);
+//
+//    @Select("SELECT * FROM sys_user WHERE username LIKE #{username} LIMIT #{pageNum}, #{pageSize}")
+//    List<SysUser> selectPage(Integer pageNum, Integer pageSize, String username);
+//
+//    @Select("SELECT COUNT(*) FROM sys_user WHERE username LIKE #{username}")
+//    Integer selectTotal(String username);
 }
