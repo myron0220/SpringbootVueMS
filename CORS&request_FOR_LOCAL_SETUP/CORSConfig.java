b381a06b396d002683d788eeb69231fb897134af
myron0220 <WXYZ0daniu@gmail.com>
2022-05-18 16:15:01 -0400

    add CORS and request config for local machine

---
+++ CORS&request_FOR_LOCAL_SETUP/CORSConfig.java
@@ -0,0 +1,29 @@
+package com.mingzhe.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CORSConfig {
+
+    // default one day for maximum request time
+    private static final long MAX_AGE = 24 * 60 * 60;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // in local machine, use:
+       corsConfiguration.addAllowedOrigin("http://localhost:8181");
+        // for replit. use:
+        // corsConfiguration.addAllowedOrigin("https://springbootvuems.myron0220.repl.co");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setMaxAge(MAX_AGE);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
---
+++ CORS&request_FOR_LOCAL_SETUP/request.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+const request = axios.create({
+    // in local machine, use:
+    baseURL: 'http://localhost:9191/',
+    // in replit, use:
+    // baseURL: 'https://springbootvuems-1.myron0220.repl.co',
+    timeout: 5000
+})
+
+request.interceptors.request.use(config => {
+    config.headers['Content-Type'] = 'application/json;charset=utf-8';
+
+    // config.headers['token'] = user.token;  // set request header
+    return config
+}, error => {
+    return Promise.reject(error)
+});
+
+request.interceptors.response.use(
+    response => {
+        let res = response.data;
+        // if returned a file type
+        if (response.config.responseType === 'blob') {
+            return res
+        }
+        // otherwise, return json
+        if (typeof res === 'string') {
+            res = res ? JSON.parse(res) : res
+        }
+        return res;
+    },
+    error => {
+        console.log('err' + error) // for debug
+        return Promise.reject(error)
+    }
+)
+
+
+export default request
+
