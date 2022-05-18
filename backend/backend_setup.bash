66227da33e4649ea8495cb521c41b6a120c53ea0
myron0220 <WXYZ0daniu@gmail.com>
2022-05-18 15:20:45 -0400

    modify backend set up

--- backend/backend_setup.bash
+++ backend/backend_setup.bash
@@ -1,4 +1,7 @@
-#!/bin/bash
+# !/bin/bash
 
 cd backend
-mvn spring-boot:run
+java -jar target/backend-0.0.1-SNAPSHOT.jar
+
+# for any case where the .jar is not correct, run the following
+# mvn spring-boot:run
